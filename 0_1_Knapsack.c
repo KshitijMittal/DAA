8cdc6cedb787c2b1c16aff2d5d181d3c8b88fd88
Kshitij <kshitijmittal2007@gmail.com>
2025-09-18 09:42:50 +0530

    Update 0_1_Knapsack.c

--- 0_1_Knapsack.c
+++ 0_1_Knapsack.c
@@ -22,22 +22,31 @@ return c[n, W]
 int max(int a, int b) {
     return (a > b) ? a : b;
 }
-int knapsack(int W, int wt[], int val[], int n) {
-    int i, w;
-    int K[MAX][MAX];
 
-    for (i = 0; i <= n; i++) {
-        for (w = 0; w <= W; w++) {
-            if (i == 0 || w == 0)
-                K[i][w] = 0;
-            else if (wt[i - 1] <= w)
-                K[i][w] = max(val[i - 1] + K[i - 1][w - wt[i - 1]], K[i - 1][w]);
-            else
-                K[i][w] = K[i - 1][w];
+int knapsack(int W, int wt[], int val[], int n) {
+    int i, l;
+    int c[MAX][MAX];  // DP table
+
+    // Initialize base cases
+    for (l = 0; l <= W; l++) {
+        c[0][l] = 0;
+    }
+    for (i = 1; i <= n; i++) {
+        c[i][0] = 0;
+        for (l = 1; l <= W; l++) {
+            if (wt[i - 1] <= l) {
+                if (val[i - 1] + c[i - 1][l - wt[i - 1]] > c[i - 1][l])
+                    c[i][l] = val[i - 1] + c[i - 1][l - wt[i - 1]];
+                else
+                    c[i][l] = c[i - 1][l];
+            } else {
+                c[i][l] = c[i - 1][l];
+            }
         }
     }
-    return K[n][W];
+    return c[n][W];
 }
+
 int main() {
     int n, W, i;
     int val[MAX], wt[MAX];
